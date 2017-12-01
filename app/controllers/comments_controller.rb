9b1410ab3526cc46674a28b37f0f069b16de3a23
Amandine Latour <amandine.latour+github@gmail.com>
2017-12-01 14:26:54 -0500

    likes destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,20 +19,38 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = Comment.find(params[:id])
     @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
   end
 
   def update
     @comment = Comment.find(params[:id])
-    if @comment.update(post_params)
+    @post = @comment.post
+    if @comment.update(comment_params)
       flash.now[:notice] = "Comment updated!"
-      redirect_to
+      respond_to do |format|
+        format.js
+      end
     else
       flash.now[:warning] = "There was an error. Please try again."
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    if @comment.destroy
+      flash.now[:notice] = "Comment deleted!"
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash.now[:warning] = "There was an error. Please try again."
+    end
+  end
 
   private
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,28 @@
 class LikesController < ApplicationController
 
+  def index
+    @post = Post.find(params[:post_id])
+    @likes = @post.likes
+  end
 
-    def create
-      @post = Post.find(params[:post_id])
-      @like = Like.new
-      @like.post = @post
-      @like.author = current_user
-      if @like.save
-        flash.now[:notice] = "Post liked !"
-        respond_to do |format|
-          format.js
-        end
-      else
-        flash.now[:warning] = "There was an error while liking a post."
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.new
+    @like.post = @post
+    @like.author = current_user
+    if @like.save
+      flash.now[:notice] = "Post liked !"
+      respond_to do |format|
+        format.js
       end
+    else
+      flash.now[:warning] = "There was an error while liking a post."
     end
+  end
 
-    def index
-      @post = Post.find(params[:post_id])
-      @likes = @post.likes
-    end
+  def destroy
+    
+  end
 
 
 end
