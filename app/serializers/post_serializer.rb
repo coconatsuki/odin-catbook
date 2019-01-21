bc4315e86a9e650177d443d82ae496ebccc08528
Amandine Latour <amandine.latour+github@gmail.com>
2019-01-21 17:30:43 +0900

    image urls migration and update post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :body, :created_at
+  attributes :id, :body, :smallImageUrl, :largeImageUrl, :created_at
   belongs_to :author, serializer: AuthorSerializer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204182153) do
+ActiveRecord::Schema.define(version: 20190121081913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,8 @@ ActiveRecord::Schema.define(version: 20171204182153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
-    t.string "picture"
+    t.string "smallImageUrl"
+    t.string "largeImageUrl"
   end
 
   create_table "users", force: :cascade do |t|
